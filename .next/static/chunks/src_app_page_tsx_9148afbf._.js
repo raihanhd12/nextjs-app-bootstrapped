72a9d93eb856abb98984dfce88f8de1b20c63b89
Raihan Hidayatullah Djunaedi <95725937+raihanhd12@users.noreply.github.com>
2025-05-14 23:07:22 +0700

    Add .next/static/chunks/src_app_page_tsx_9148afbf._.js 1747238841536

---
+++ .next/static/chunks/src_app_page_tsx_9148afbf._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_71dc3fa1._.js",
+  "static/chunks/src_4de5d0c7._.js"
+],
+    source: "dynamic"
+});
